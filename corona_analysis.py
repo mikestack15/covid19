ed744bde922b7cb236cda93493447f7ae328dd05
mikestack15 <mikestack15@gmail.com>
2020-04-04 16:42:09 -0500

    nan handling

--- corona_analysis.py
+++ corona_analysis.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from datetime import datetime
 from datetime import timedelta
+pd.set_option('display.max_columns', None)
 
 #read the most recent data from today (ideal to run in the evening)
 
@@ -11,6 +12,25 @@ report_date = today_date.strftime('%m-%d-%Y')
 
 daily_file_url = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/' + report_date + '.csv'
 covid_data_daily_file = pd.read_csv(daily_file_url)
+
+#select only relevant columns
+covid_data = covid_data_daily_file[['Combined_Key','Confirmed','Deaths','Recovered','Active','Lat',
+                       'Long_','Country_Region','Province_State']]
+
+us_covid_data_all = covid_data[covid_data['Country_Region'] == 'US']
+
+#handle for rows with unassigned origin locations; capture aggregates in case they want to be used in later
+#analysis
+is_NaN = us_covid_data_all.isnull()
+row_has_NaN = is_NaN.any(axis=1)
+confirmed_cases_unassigned = us_covid_data_all[row_has_NaN]['Confirmed'].sum()
+deaths_unassigned = us_covid_data_all[row_has_NaN]['Deaths'].sum()
+recovered_unassigned = us_covid_data_all[row_has_NaN]['Recovered'].sum()
+
+us_covid_data = us_covid_data_all.dropna(axis=0)
+
+
+
 
 
 #confirmed cases
@@ -33,7 +53,16 @@ covid_global_deaths = pd.read_csv(deaths_global_url)
 
 ### Aggregations
 
-
+#Stacked (dual-axis graphs) [cases x deaths, time series]
+#-calculate factor/ratio/rate of delay for cases -> (to) deaths
+#-python integration
+#Geographical map of deaths
+#-filter case, deaths, cases to death ratio, per 1M deaths/cases/cases to death ratio
+#-forecast of cases, deaths, and different rate metrics
+#-distance traveled metric, logarithmic easing (rate of decreasing cases, prediction of “Peter out” threshold
+#-5 day trailing moving averages
+#-temperature, humidity, UV exposure as a feature?
+#-is active cases mapped across a times series a predictor of future deaths? At what rate?
 
 
 
